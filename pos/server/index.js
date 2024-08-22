abdcd305ef9400b3aab11d2e6dd0773532fa3b4a
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2024-08-22 12:16:12 +0530

    replaced nodemon with node in server

--- pos/server/index.js
+++ pos/server/index.js
@@ -9,7 +9,9 @@ const cors = require('cors');
 const app=express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  
+))
 
 mongoose.connect("mongodb://127.0.0.1:27017/pos",{
     useNewUrlParser: true,
