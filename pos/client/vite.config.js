6a0be5df6980487fa947081bca49ade48d65552d
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2024-08-22 16:46:46 +0530

    modified vite.config

--- pos/client/vite.config.js
+++ pos/client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['chart.js', 'react-chartjs-2'], // Pre-bundle these dependencies during dev
+  },
   build: {
     rollupOptions: {
       // Ensure this external option is correctly defined or remove it if not needed
