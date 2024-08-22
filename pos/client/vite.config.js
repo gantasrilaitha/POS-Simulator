0051c3202fad103fea14d6a06e5c6216e3cfa223
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2024-08-22 16:50:00 +0530

    modified vite.config

--- pos/client/vite.config.js
+++ pos/client/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['chart.js', 'react-chartjs-2'], // Pre-bundle these dependencies during dev
+  },
   build: {
     rollupOptions: {
       // Ensure this external option is correctly defined or remove it if not needed
-      external: [['react', 'react-dom']], // Specify external dependencies here or remove this line if you want everything bundled
+      external: [['react', 'react-dom','chart','react-chartjs-2']], // Specify external dependencies here or remove this line if you want everything bundled
     },
   },
 });
