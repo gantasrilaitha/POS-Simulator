e816709a26631e257181c78343d339ccd589fae0
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2024-08-22 16:34:21 +0530

    modified vite.config

--- pos/client/vite.config.js
+++ pos/client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Ensure this external option is correctly defined or remove it if not needed
+      external: [['react', 'react-dom']], // Specify external dependencies here or remove this line if you want everything bundled
+    },
+  },
 });
